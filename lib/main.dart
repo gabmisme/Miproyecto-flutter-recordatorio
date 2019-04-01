29fdb4a2d09ef2749c66ad95e02e4355f560e82a
Gabriel Limachi Misme <gabrielmisme@gmail.com>
2019-03-31 19:33:39 -0700

    cambio de color a los botones

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,10 @@ void main() => runApp(MaterialApp(
       theme: ThemeData.dark(),
       home: MyrecorApp(),
     ));
-
 class MyrecorApp extends StatefulWidget {
   @override
   _MyrecorApp createState() => new _MyrecorApp();
 }
-
 class _MyrecorApp extends State<MyrecorApp> {
   @override
   Widget build(BuildContext context) {
@@ -21,42 +19,61 @@ class _MyrecorApp extends State<MyrecorApp> {
         title: Text(
           'RECORDATORIO',
           style: TextStyle(
-              color: Colors.white, fontSize: 25.0, fontWeight: FontWeight.bold),
+              color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.w200),
         ),
-        centerTitle: true,
+         // backgroundColor: new Color(0xFF673AB7),
+        //centerTitle: true,
       ),
+
       body: Scaffold(
         body: Container(
+          //color: Color(0xFF673AB7),
           child: new Column(
+
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
+          Row(
+
+             mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+
                   new RaisedButton(
-                      color: Colors.white,
-                      textColor: Colors.black,
+                      padding: const EdgeInsets.all(0.90),//
+                      color: Colors.black,
+                      textColor: Colors.white,
                       child: Text(
                         "MENU",
                         style: TextStyle(
-                          fontSize: 20.0,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      onPressed: () {}),
+                  new RaisedButton(
+                      padding: const EdgeInsets.all(0.90),//
+                      color: Colors.black,
+                      textColor: Colors.white,
+                      child: Text(
+                        "AGENDA",
+                        style: TextStyle(
+                          fontSize: 15.0,
                         ),
                       ),
                       onPressed: () {}),
 
                   new RaisedButton(
-
-                      color: Colors.white,
-                      textColor: Colors.black,
+                      padding: const EdgeInsets.all(0.90),//
+                      color: Colors.black,
+                      textColor: Colors.white,
                       child: Text(
-                        "AGENDA",
+                        "AÑADIR",
                         style: TextStyle(
-                          fontSize: 20.0,
+                          fontSize: 15.0,
                         ),
                       ),
                       onPressed: () {}),
                 ],
 
-              )
+              ),
             ],
           ),
         ),
