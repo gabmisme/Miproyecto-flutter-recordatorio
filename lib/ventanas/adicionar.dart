d3f10c3f2e6088652632bd3642070d9e6b0598f2
Gabriel Limachi Misme <gabrielmisme@gmail.com>
2019-04-05 22:11:09 -0700

    modificando ventana de añadir barra app

--- lib/ventanas/adicionar.dart
+++ lib/ventanas/adicionar.dart
@@ -5,6 +5,16 @@ class SecondScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text(
+          'RECORDATORIO',
+          style: TextStyle(
+              color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.w200),
+        ),
+        // backgroundColor: new Color(0xFF673AB7),
+        //centerTitle: true,
+      ),
 
     );
   }
