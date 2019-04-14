ceadabe34975375fabaa9e16620752d01e04af74
Gabriel Limachi Misme <gabrielmisme@gmail.com>
2019-04-14 00:21:27 -0700

    lista de texto-ventana Mas Opciones

--- lib/ventanas/MasOpciones.dart
+++ lib/ventanas/MasOpciones.dart
@@ -12,7 +12,42 @@ class MyDrawer01 extends StatelessWidget{
       child: Container(
         child:new Stack(
           children: <Widget>[
-
+            //menu desplegable
+            Positioned(
+              left:20.0,
+              right:25.0,
+              top:50.0,
+              child: SizedBox(
+                width:400.0,
+                height:300.0,
+                child: Container(
+                  /******************lista**********************/
+                  child: ListView(
+                    //padding: EdgeInsets.zero,
+                    children: <Widget>[
+                      /*
+                      new DrawerHeader(
+                        child: Text('Drawer Headre'),
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      */
+                      Ink(
+                        //color: Colors.blue,
+                        child: new ListTile(title: Text('men 2'),),
+                      ),
+                      ListTile(
+                        title: Text('Salir',style: TextStyle(color:Colors.white,fontSize:20.0),),
+                        onTap:(){Navigator.pop(context);},
+                      ),
+                    ],
+                  ),
+                  /****************fin lista ***********************************************/
+                ),
+              ),
+            ),
+        /************************/
           ],
         ),
       ),
